5962fd3b785e7a032de2d81ee123bd1fe39d139e
zhe.liu <sking.pt@gmail.com>
2015-09-10 18:06:07 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var curl = require('./lib/curl');
 var clean = require('./lib/extract');
 curl('http://blog.rainy.im/2015/09/02/web-content-and-main-image-extractor/', function (content) {
+    console.log('fetch done');
     clean(content, {
-        blockSize: 5
+        blockSize: 3
     });
 });
 
