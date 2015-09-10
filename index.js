d35aa06c57b1d6de699bca9292f12942173511f2
zhe.liu <sking.pt@gmail.com>
2015-09-10 17:27:13 +0800

    update block size

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var curl = require('./lib/curl');
 var clean = require('./lib/extract');
-curl('https://cnodejs.org/topic/53142ef833dbcb076d007230', function (content) {
+curl('http://blog.rainy.im/2015/09/02/web-content-and-main-image-extractor/', function (content) {
     clean(content, {
-        blockSize: 3
+        blockSize: 5
     });
 });
 
